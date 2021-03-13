9b7ab4f0a5ece1199496b2f79500c0b142211aaa
erri120 <erri120@protonmail.com>
2021-03-13 17:01:03 +0100

    Add Example project

---
+++ GameFinder.Example/Options.cs
@@ -0,0 +1,22 @@
+﻿using CommandLine;
+
+namespace GameFinder.Example
+{
+    public class Options
+    {
+        [Option("bethnet", HelpText = "Search for BethNet games")]
+        public bool BethNet { get; set; } = true;
+
+        [Option("egs", HelpText = "Search for EGS games")]
+        public bool EGS { get; set; } = true;
+
+        [Option("gog", HelpText = "Search for GOG games")]
+        public bool GOG { get; set; } = true;
+
+        [Option("steam", HelpText = "Search for Steam games")]
+        public bool Steam { get; set; } = true;
+
+        [Option("xbox", HelpText = "Search for Xbox games")]
+        public bool Xbox { get; set; } = true;
+    }
+}
---
+++ GameFinder.Example/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using CommandLine;
+using GameFinder.StoreHandlers.BethNet;
+using GameFinder.StoreHandlers.EGS;
+using GameFinder.StoreHandlers.GOG;
+using GameFinder.StoreHandlers.Steam;
+using GameFinder.StoreHandlers.Xbox;
+
+namespace GameFinder.Example
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(Run);
+        }
+
+        private static void Log(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        private static void RunHandler<THandler, TGame>(string handlerName, Action<TGame> logGame) where THandler : AStoreHandler<TGame>, new() where TGame : AStoreGame
+        {
+            Log($"Finding {handlerName} games...");
+            try
+            {
+                var handler = new THandler();
+                if (!handler.FindAllGames())
+                {
+                    Log($"Unable to find {handlerName} games");
+                    return;
+                }
+                Log($"Found {handler.Games.Count} {handlerName} games");
+                handler.Games.ForEach(logGame);
+            }
+            catch (Exception e)
+            {
+                Log($"Exception trying to find {handlerName} games:\n{e}");
+            }
+        }
+        
+        private static void Run(Options options)
+        {
+            if (options.BethNet)
+            {
+                RunHandler<BethNetHandler, BethNetGame>("BethNet", 
+                    x => Log($"{x}: {x.Path}"));
+            }
+
+            if (options.EGS)
+            {
+                RunHandler<EGSHandler, EGSGame>("EGS",
+                    x => Log($"{x}: {x.Path}"));
+            }
+
+            if (options.GOG)
+            {
+                RunHandler<GOGHandler, GOGGame>("GOG",
+                    x => Log($"{x}: {x.Path}"));
+            }
+
+            if (options.Steam)
+            {
+                RunHandler<SteamHandler, SteamGame>("Steam",
+                    x => Log($"{x}: {x.Path}"));
+            }
+
+            if (options.Xbox)
+            {
+                RunHandler<XboxHandler, XboxGame>("Xbox",
+                    x => Log($"{x}: {x.Path}"));
+            }
+        }
+    }
+}
