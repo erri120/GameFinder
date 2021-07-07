da54108ca91c273cf85979c22eaa61c4cee79d1e
erri120 <erri120@protonmail.com>
2021-07-07 20:30:46 +0200

    Update OriginHandler

--- GameFinder.StoreHandlers.Origin/OriginHandler.cs
+++ GameFinder.StoreHandlers.Origin/OriginHandler.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using JetBrains.Annotations;
 
 namespace GameFinder.StoreHandlers.Origin
 {
@@ -6,6 +7,25 @@ namespace GameFinder.StoreHandlers.Origin
     public class OriginHandler : AStoreHandler<OriginGame>
     {
         public override StoreType StoreType => StoreType.Origin;
+
+        private readonly bool _useLocalFiles;
+        private readonly bool _useApi;
+
+        private readonly string? _localContentPath;
+        
+        public OriginHandler(bool useLocalFiles, bool useApi)
+        {
+            _useLocalFiles = useLocalFiles;
+            _useApi = useApi;
+
+            if (!_useLocalFiles && !_useApi)
+                throw new ArgumentException("Local ");
+        }
+
+        public OriginHandler(string localContentPath, bool useLocalFiles, bool useApi) : this(useLocalFiles, useApi)
+        {
+            _localContentPath = localContentPath;
+        }
         
         public override Result<bool> FindAllGames()
         {
