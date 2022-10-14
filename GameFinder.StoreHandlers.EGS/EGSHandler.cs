1913dde6a234db052ff50d1f5cf41e72f03d928c
erri120 <erri120@protonmail.com>
2022-10-14 17:17:34 +0200

    Update docs

--- GameFinder.StoreHandlers.EGS/EGSHandler.cs
+++ GameFinder.StoreHandlers.EGS/EGSHandler.cs
@@ -10,9 +10,18 @@ using JetBrains.Annotations;
 
 namespace GameFinder.StoreHandlers.EGS;
 
+/// <summary>
+/// Represents a game installed with the Epic Games Store.
+/// </summary>
+/// <param name="CatalogItemId"></param>
+/// <param name="DisplayName"></param>
+/// <param name="InstallLocation"></param>
 [PublicAPI]
 public record EGSGame(string CatalogItemId, string DisplayName, string InstallLocation);
 
+/// <summary>
+/// Handler for finding games installed with the Epic Games Store.
+/// </summary>
 [PublicAPI]
 public class EGSHandler
 {
@@ -21,17 +30,37 @@ public class EGSHandler
     private readonly IRegistry _registry;
     private readonly IFileSystem _fileSystem;
     
+    /// <summary>
+    /// Default constructor. This uses the <see cref="WindowsRegistry"/> implementation of
+    /// <see cref="IRegistry"/> and the real file system with <see cref="FileSystem"/>.
+    /// </summary>
     [SupportedOSPlatform("windows")]
     public EGSHandler() : this(new WindowsRegistry(), new FileSystem()) { }
     
+    /// <summary>
+    /// Constructor for specifying the implementation of <see cref="IRegistry"/>. This uses
+    /// the real file system with <see cref="FileSystem"/>.
+    /// </summary>
+    /// <param name="registry"></param>
     public EGSHandler(IRegistry registry) : this(registry, new FileSystem()) { }
     
+    /// <summary>
+    /// Constructor for specifying the implementation of <see cref="IRegistry"/> and
+    /// <see cref="IFileSystem"/> when doing tests.
+    /// </summary>
+    /// <param name="registry"></param>
+    /// <param name="fileSystem"></param>
     public EGSHandler(IRegistry registry, IFileSystem fileSystem)
     {
         _registry = registry;
         _fileSystem = fileSystem;
     }
     
+    /// <summary>
+    /// Finds all games installed with the Epic Games Store. This generator function will always return
+    /// either a non-null <see cref="EGSGame"/> or a non-null error message.
+    /// </summary>
+    /// <returns></returns>
     public IEnumerable<(EGSGame? game, string? error)> FindAllGames()
     {
         var manifestDir = _fileSystem.DirectoryInfo.FromDirectoryName(GetManifestDir());
