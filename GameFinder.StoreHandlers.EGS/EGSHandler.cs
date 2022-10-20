64983b137874feacec41492844dc03b0e159c9d5
erri120 <erri120@protonmail.com>
2022-10-20 17:41:23 +0200

    Update EGS handler and tests

--- GameFinder.StoreHandlers.EGS/EGSHandler.cs
+++ GameFinder.StoreHandlers.EGS/EGSHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.IO.Abstractions;
+using System.Linq;
 using System.Runtime.Versioning;
 using System.Text.Json;
 using GameFinder.RegistryUtils;
@@ -25,25 +26,40 @@ public record EGSGame(string CatalogItemId, string DisplayName, string InstallLo
 [PublicAPI]
 public class EGSHandler
 {
-    private const string RegKey = @"Software\Epic Games\EOS";
-    
+    /// <summary>
+    /// Represents the return values of <see cref="EGSHandler.FindAllGames"/>. This
+    /// record will either contain a non-null <see cref="EGSGame"/> or a non-null error
+    /// message.
+    /// </summary>
+    /// <param name="Game"></param>
+    /// <param name="Error"></param>
+    public readonly record struct Result(EGSGame? Game, string? Error);
+
+    internal const string RegKey = @"Software\Epic Games\EOS";
+
     private readonly IRegistry _registry;
     private readonly IFileSystem _fileSystem;
-    
+
+    private readonly JsonSerializerOptions _jsonSerializerOptions =
+        new()
+        {
+            AllowTrailingCommas = true
+        };
+
     /// <summary>
     /// Default constructor. This uses the <see cref="WindowsRegistry"/> implementation of
     /// <see cref="IRegistry"/> and the real file system with <see cref="FileSystem"/>.
     /// </summary>
     [SupportedOSPlatform("windows")]
     public EGSHandler() : this(new WindowsRegistry(), new FileSystem()) { }
-    
+
     /// <summary>
     /// Constructor for specifying the implementation of <see cref="IRegistry"/>. This uses
     /// the real file system with <see cref="FileSystem"/>.
     /// </summary>
     /// <param name="registry"></param>
     public EGSHandler(IRegistry registry) : this(registry, new FileSystem()) { }
-    
+
     /// <summary>
     /// Constructor for specifying the implementation of <see cref="IRegistry"/> and
     /// <see cref="IFileSystem"/> when doing tests.
@@ -55,68 +71,106 @@ public class EGSHandler
         _registry = registry;
         _fileSystem = fileSystem;
     }
-    
+
     /// <summary>
-    /// Finds all games installed with the Epic Games Store. This function will always return
-    /// either a non-null <see cref="EGSGame"/> or a non-null error message.
+    /// Finds all games installed with the Epic Games Store.
     /// </summary>
     /// <returns></returns>
-    public IEnumerable<(EGSGame? game, string? error)> FindAllGames()
+    public IEnumerable<Result> FindAllGames()
     {
         var manifestDir = _fileSystem.DirectoryInfo.FromDirectoryName(GetManifestDir());
         if (!manifestDir.Exists)
         {
-            yield return (null, $"The manifest directory {manifestDir.FullName} does not exist!");
+            yield return new Result(null, $"The manifest directory {manifestDir.FullName} does not exist!");
             yield break;
         }
 
-        var itemFiles = manifestDir.EnumerateFiles("*.item", SearchOption.TopDirectoryOnly);
+        var itemFiles = manifestDir
+            .EnumerateFiles("*.item", SearchOption.TopDirectoryOnly)
+            .ToArray();
+
+        if (itemFiles.Length == 0)
+        {
+            yield return new Result(null, $"The manifest directory {manifestDir.FullName} does not contain any .item files");
+            yield break;
+        }
+
         foreach (var itemFile in itemFiles)
         {
             yield return DeserializeGame(itemFile);
         }
     }
 
-    private (EGSGame? game, string? error) DeserializeGame(IFileInfo itemFile)
+    private Result DeserializeGame(IFileInfo itemFile)
     {
         using var stream = itemFile.OpenRead();
 
         try
         {
-            var game = JsonSerializer.Deserialize<EGSGame>(stream);
+            var game = JsonSerializer.Deserialize<EGSGame>(stream, _jsonSerializerOptions);
+
             if (game is null)
             {
-                return (null, $"Unable to deserialize file {itemFile.FullName}");
+                return new Result(null, $"Unable to deserialize file {itemFile.FullName}");
             }
 
-            return (game, null);
+            // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+            if (game.CatalogItemId is null)
+            {
+                return new Result(null, $"Manifest {itemFile.FullName} does not have a value \"CatalogItemId\"");
+            }
+
+            // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+            if (game.DisplayName is null)
+            {
+                return new Result(null, $"Manifest {itemFile.FullName} does not have a value \"DisplayName\"");
+            }
+
+            // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+            if (game.InstallLocation is null)
+            {
+                return new Result(null, $"Manifest {itemFile.FullName} does not have a value \"InstallLocation\"");
+            }
+
+            return new Result(game, null);
         }
         catch (Exception e)
         {
-            return (null, $"Unable to deserialize file {itemFile.FullName}:\n{e}");
+            return new Result(null, $"Unable to deserialize file {itemFile.FullName}:\n{e}");
         }
     }
-    
+
     private string GetManifestDir()
     {
-        return TryGetManifestDirFromRegistry(out var manifestDir) 
-            ? manifestDir 
-            : _fileSystem.Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
-                "Epic",
-                "EpicGamesLauncher",
-                "Data",
-                "Manifests");
+        return TryGetManifestDirFromRegistry(out var manifestDir)
+            ? manifestDir
+            : GetDefaultManifestsPath(_fileSystem);
+    }
+
+    internal static string GetDefaultManifestsPath(IFileSystem fileSystem)
+    {
+        return fileSystem.Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
+            "Epic",
+            "EpicGamesLauncher",
+            "Data",
+            "Manifests");
     }
 
     private bool TryGetManifestDirFromRegistry([MaybeNullWhen(false)] out string manifestDir)
     {
         manifestDir = default;
-        
-        var currentUser = _registry.OpenBaseKey(RegistryHive.CurrentUser);
-        using var regKey = currentUser.OpenSubKey(RegKey);
 
-        return regKey is not null && regKey.TryGetString("ModSdkMetadataDir", out manifestDir);
+        try
+        {
+            var currentUser = _registry.OpenBaseKey(RegistryHive.CurrentUser);
+            using var regKey = currentUser.OpenSubKey(RegKey);
+
+            return regKey is not null && regKey.TryGetString("ModSdkMetadataDir", out manifestDir);
+        }
+        catch (Exception)
+        {
+            return false;
+        }
     }
 }
-
--- GameFinder.Tests/EGSTests.cs
+++ GameFinder.Tests/EGSTests.cs
@@ -1,68 +1,181 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.IO.Abstractions.TestingHelpers;
 using System.Linq;
-using System.Runtime.InteropServices;
 using GameFinder.RegistryUtils;
 using GameFinder.StoreHandlers.EGS;
 using Xunit;
 
 namespace GameFinder.Tests;
 
+[SuppressMessage("ReSharper", "ParameterOnlyUsedForPreconditionCheck.Local")]
 public class EGSTests
 {
-    private static (InMemoryRegistry registry, MockFileSystem fileSystem, List<EGSGame> expectedGames) SetupTest()
+    [Fact]
+    public void Test_ShouldWork()
     {
-        var expectedGames = new List<EGSGame>
+        var fs = new MockFileSystem();
+
+        var manifestDir = fs.Path.GetTempPath();
+
+        var expectedGames = new EGSGame[]
         {
             new("b8538c739273426aa35a98220e258d55", "Unreal Tournament", "C:\\Games\\UnrealTournament"),
             new("3257e06c28764231acd93049f3774ed6", "Subnautica", "C:\\Games\\Subnautica")
         };
-        
-        var registry = new InMemoryRegistry();
 
-        var regKey = registry.AddKey(RegistryHive.CurrentUser, @"Software\Epic Games\EOS");
-
-        var baseDir = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-            ? "C:\\ModSdkMetadataDir"
-            : "/ModSdkMetadataDir";
-        
-        regKey.AddValue("ModSdkMetadataDir", baseDir);
-        
-        var fileSystem = new MockFileSystem();
-
-        foreach (var expectedGame in expectedGames)
+        foreach (var game in expectedGames)
         {
             var mockData = @$"{{
-    ""CatalogItemId"": ""{expectedGame.CatalogItemId}"",
-    ""DisplayName"": ""{expectedGame.DisplayName}"",
-    ""InstallLocation"": ""{expectedGame.InstallLocation.Replace("\\", "\\\\")}""
+    ""CatalogItemId"": ""{game.CatalogItemId}"",
+    ""DisplayName"": ""{game.DisplayName}"",
+    ""InstallLocation"": ""{game.InstallLocation.Replace("\\", "\\\\")}""
 }}";
 
-            fileSystem.AddFile($"{Path.Combine(baseDir, $"{expectedGame.CatalogItemId}.item")}", new MockFileData(mockData));
+            fs.AddFile($"{fs.Path.Combine(manifestDir, $"{game.CatalogItemId}.item")}", mockData);
         }
 
-        return (registry, fileSystem, expectedGames);
-    }
+        var registry = new InMemoryRegistry();
 
-    [Fact]
-    private void TestFindAllGames()
-    {
-        var (registry, fileSystem, expectedGames) = SetupTest();
+        var regKey = registry.AddKey(RegistryHive.CurrentUser, EGSHandler.RegKey);
+        regKey.AddValue("ModSdkMetadataDir", manifestDir);
 
-        var handler = new EGSHandler(registry, fileSystem);
-        
-        var results = handler.FindAllGames().ToList();
-        
+        var handler = new EGSHandler(registry, fs);
+
+        var results = handler.FindAllGames().ToArray();
         var actualGames = results.Select(tuple =>
         {
             var (game, error) = tuple;
             Assert.True(game is not null, error);
             return game;
-        }).ToList();
-        
-        Assert.Equal(expectedGames.Count, actualGames.Count);
+        }).ToArray();
+
+        Assert.Equal(expectedGames.Length, actualGames.Length);
         Assert.Equal(expectedGames, actualGames);
     }
-}
 
+    [Fact]
+    public void Test_ShouldError_MissingDirectory_DefaultPath()
+    {
+        var registry = new InMemoryRegistry();
+        var fs = new MockFileSystem();
+
+        var handler = new EGSHandler(registry, fs);
+
+        var results = handler.FindAllGames().ToArray();
+        Assert.Collection(results, result =>
+        {
+            Assert.Null(result.Game);
+            Assert.Equal($"The manifest directory {EGSHandler.GetDefaultManifestsPath(fs)} does not exist!", result.Error);
+        });
+    }
+
+    [Fact]
+    public void Test_ShouldError_MissingDirectory_Registry()
+    {
+        var registry = new InMemoryRegistry();
+
+        var regKey = registry.AddKey(RegistryHive.CurrentUser, EGSHandler.RegKey);
+        regKey.AddValue("ModSdkMetadataDir", "C:\\foo");
+
+        var fs = new MockFileSystem();
+
+        var handler = new EGSHandler(registry, fs);
+
+        var results = handler.FindAllGames().ToArray();
+        Assert.Collection(results, result =>
+        {
+            Assert.Null(result.Game);
+            Assert.Equal("The manifest directory C:\\foo does not exist!", result.Error);
+        });
+    }
+
+    [Fact]
+    public void Test_ShouldError_InvalidManifest()
+    {
+        var fs = new MockFileSystem();
+
+        var manifestDir = fs.Path.GetTempPath();
+
+        var randomBytes = new byte[128];
+        Random.Shared.NextBytes(randomBytes);
+
+        fs.AddFile(fs.Path.Combine(manifestDir, "brokenManifest.item"), new MockFileData(randomBytes));
+
+        var registry = new InMemoryRegistry();
+
+        var regKey = registry.AddKey(RegistryHive.CurrentUser, EGSHandler.RegKey);
+        regKey.AddValue("ModSdkMetadataDir", manifestDir);
+
+        var handler = new EGSHandler(registry, fs);
+
+        var results = handler.FindAllGames().ToArray();
+        Assert.Collection(results, result =>
+        {
+            Assert.Null(result.Game);
+            Assert.StartsWith("Unable to deserialize file", result.Error);
+        });
+    }
+
+    [Fact]
+    public void Test_ShouldError_MissingValues()
+    {
+        var fs = new MockFileSystem();
+
+        var manifestDir = fs.Path.GetTempPath();
+
+        var manifest1 = fs.Path.Combine(manifestDir, "no-id.item");
+        var manifest2 = fs.Path.Combine(manifestDir, "no-name.item");
+        var manifest3 = fs.Path.Combine(manifestDir, "no-path.item");
+
+        fs.AddFile(manifest1, "{}");
+        fs.AddFile(manifest2, @"{ ""CatalogItemId"": ""foo"" }");
+        fs.AddFile(manifest3, @"{ ""CatalogItemId"": ""foo"", ""DisplayName"": ""bar"" }");
+
+        var registry = new InMemoryRegistry();
+
+        var regKey = registry.AddKey(RegistryHive.CurrentUser, EGSHandler.RegKey);
+        regKey.AddValue("ModSdkMetadataDir", manifestDir);
+
+        var handler = new EGSHandler(registry, fs);
+
+        var results = handler.FindAllGames().ToArray();
+        Assert.Collection(results, result =>
+        {
+            Assert.Null(result.Game);
+            Assert.Equal($"Manifest {manifest1} does not have a value \"CatalogItemId\"", result.Error);
+        }, result =>
+        {
+            Assert.Null(result.Game);
+            Assert.Equal($"Manifest {manifest2} does not have a value \"DisplayName\"", result.Error);
+        }, result =>
+        {
+            Assert.Null(result.Game);
+            Assert.Equal($"Manifest {manifest3} does not have a value \"InstallLocation\"", result.Error);
+        });
+    }
+
+    [Fact]
+    public void Test_ShouldError_NoManifests()
+    {
+        var fs = new MockFileSystem();
+
+        var manifestDir = fs.Path.GetTempPath();
+
+        fs.AddDirectory(manifestDir);
+
+        var registry = new InMemoryRegistry();
+
+        var regKey = registry.AddKey(RegistryHive.CurrentUser, EGSHandler.RegKey);
+        regKey.AddValue("ModSdkMetadataDir", manifestDir);
+
+        var handler = new EGSHandler(registry, fs);
+
+        var results = handler.FindAllGames().ToArray();
+        Assert.Collection(results, result =>
+        {
+            Assert.Null(result.Game);
+            Assert.Equal($"The manifest directory {manifestDir} does not contain any .item files", result.Error);
+        });
+    }
+}
