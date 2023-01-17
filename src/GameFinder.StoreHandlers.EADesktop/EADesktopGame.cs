ff298c9c9da8ed454815bbb84b268f202e2df728
erri120 <erri120@protonmail.com>
2023-01-17 12:10:12 +0100

    Remove InstallInfoFilePath from EADesktopGame

--- src/GameFinder.StoreHandlers.EADesktop/EADesktopGame.cs
+++ src/GameFinder.StoreHandlers.EADesktop/EADesktopGame.cs
@@ -3,8 +3,7 @@
 namespace GameFinder.StoreHandlers.EADesktop;
 
 [PublicAPI]
-public record EADesktopGame(string SoftwareID, string BaseSlug, string BaseInstallPath,
-    string InstallCheck, string InstallInfoFilePath)
+public record EADesktopGame(string SoftwareID, string BaseSlug, string BaseInstallPath, string InstallCheck)
 {
     // TODO: add helper method to run install check
     // TODO: add helper method to get installerdata.xml file
