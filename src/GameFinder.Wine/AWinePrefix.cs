b6ba6b2b04fd7a46f77c5ccbe4aaa2945942da36
erri120 <erri120@protonmail.com>
2023-03-23 13:53:50 -0700

    Wine path mappings

--- src/GameFinder.Wine/AWinePrefix.cs
+++ src/GameFinder.Wine/AWinePrefix.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using GameFinder.RegistryUtils;
 using JetBrains.Annotations;
 using NexusMods.Paths;
 
@@ -48,5 +51,62 @@ public abstract class AWinePrefix
     public AbsolutePath GetUserRegistryFile()
     {
         return ConfigurationDirectory.CombineUnchecked("user.reg");
+    }
+
+    /// <summary>
+    /// Creates an overlay <see cref="IFileSystem"/> with path
+    /// mappings into the wine prefix.
+    /// </summary>
+    /// <param name="fileSystem"></param>
+    /// <returns></returns>
+    public IFileSystem CreateOverlayFileSystem(IFileSystem fileSystem)
+    {
+        var rootDirectory = GetVirtualDrivePath();
+
+        var pathMappings = new Dictionary<AbsolutePath, AbsolutePath>
+        {
+            { fileSystem.FromFullPath("/c"), rootDirectory },
+        };
+
+        // TODO: the Wine user can be different, Proton uses "steamuser"
+        var newHomeDirectory = rootDirectory
+            .CombineUnchecked("Users")
+            .CombineUnchecked(Environment.GetEnvironmentVariable("USER")!);
+
+        var knownPaths = Enum.GetValues<KnownPath>();
+        foreach (var knownPath in knownPaths)
+        {
+            var originalPath = fileSystem.GetKnownPath(knownPath);
+            var newPath = knownPath switch
+            {
+                KnownPath.EntryDirectory => originalPath,
+                KnownPath.CurrentDirectory => originalPath,
+
+                KnownPath.CommonApplicationDataDirectory => rootDirectory.CombineUnchecked("ProgramData"),
+
+                KnownPath.HomeDirectory => newHomeDirectory,
+                KnownPath.MyDocumentsDirectory => newHomeDirectory.CombineUnchecked("Documents"),
+                KnownPath.MyGamesDirectory => newHomeDirectory.CombineUnchecked("Documents/My Games"),
+                KnownPath.LocalApplicationDataDirectory => newHomeDirectory.CombineUnchecked("AppData/Local"),
+                KnownPath.ApplicationDataDirectory => newHomeDirectory.CombineUnchecked("AppData/Roaming"),
+                KnownPath.TempDirectory => newHomeDirectory.CombineUnchecked("AppData/Local/Temp"),
+            };
+
+            pathMappings[originalPath] = newPath;
+        }
+
+        return fileSystem.CreateOverlayFileSystem(pathMappings, true);
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="IRegistry"/> implementation,
+    /// based on the registry files in the configuration
+    /// directory.
+    /// </summary>
+    /// <returns></returns>
+    public IRegistry CreateRegistry()
+    {
+        // TODO: this entire method
+        return new InMemoryRegistry();
     }
 }
