cde536c5efa1bee0789452fab4b88feeeba75ea3
erri120 <erri120@protonmail.com>
2023-04-12 12:09:46 +0200

    Update skip

--- tests/GameFinder.Common.Tests/HandlerTests.cs
+++ tests/GameFinder.Common.Tests/HandlerTests.cs
@@ -17,7 +17,7 @@ public class HandlerTests
     [InlineData("C:\\", "C:\\", false)]
     public void Test_SanitizeInputPath(string input, string expected, bool linux)
     {
-        Skip.If(OperatingSystem.IsLinux() && !linux);
+        Skip.IfNot(OperatingSystem.IsLinux() && linux);
 
         var actual = Utils.SanitizeInputPath(input);
         actual.Should().Be(expected);
