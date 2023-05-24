d8ed42194f7a7facb7a44603fca3d41386de3de4
Nutzzz <enussbaum@hotmail.com>
2023-05-24 10:34:41 -0700

    Add key for decryption of IS_Nutzzz.encrypted

--- tests/GameFinder.StoreHandlers.EADesktop.Tests/CryptoTests/Test_Decryption.cs
+++ tests/GameFinder.StoreHandlers.EADesktop.Tests/CryptoTests/Test_Decryption.cs
@@ -16,6 +16,25 @@ public partial class CryptoTests
 
         var iv = Decryption.CreateDecryptionIV();
 
+        var cipherText = File.ReadAllBytes(Path.Combine("files", "IS_erri120.encrypted"));
+        var expectedPlainText = File.ReadAllText(Path.Combine("files", "IS_erri120.decrypted"));
+
+        var plainText = Decryption.DecryptFile(cipherText, key, iv);
+        plainText.Should().Be(expectedPlainText);
+    }
+
+    [Fact]
+    public void Test_Decryption2()
+    {
+        var key = new byte[]
+        {
+            0x46, 0xa8, 0x96, 0x5f, 0xa5, 0xfa, 0x90, 0xcd, 0x45, 0x8b, 0xab, 0x85, 0xfe,
+            0xa9, 0x40, 0xc5, 0x25, 0xd2, 0xc8, 0x6a, 0x95, 0x9f, 0xa3, 0xe1, 0x7f, 0x82,
+            0xec, 0x18, 0xa3, 0x8e, 0x15, 0x86,
+        };
+
+        var iv = Decryption.CreateDecryptionIV();
+
         var cipherText = File.ReadAllBytes(Path.Combine("files", "IS_Nutzzz.encrypted"));
         var expectedPlainText = File.ReadAllText(Path.Combine("files", "IS_Nutzzz.decrypted"));
 
