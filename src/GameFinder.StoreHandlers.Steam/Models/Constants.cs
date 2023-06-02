bf054f6bac8d2299f58664a521dc371d3a8a5a6a
erri120 <erri120@protonmail.com>
2023-06-02 12:20:54 +0200

    Update constants

--- src/GameFinder.StoreHandlers.Steam/Models/Constants.cs
+++ src/GameFinder.StoreHandlers.Steam/Models/Constants.cs
@@ -3,5 +3,6 @@ namespace GameFinder.StoreHandlers.Steam.Models;
 internal static class Constants
 {
     public const string SteamDbBaseUrl = "https://steamdb.info";
+    public const string SteamStoreBaseUrl = "https://store.steampowered.com";
     public const string SteamCommunityBaseUrl = "https://steamcommunity.com";
 }
