682e9f30b94400cc00ba2e9a6ed8c741002b00a0
erri120 <erri120@protonmail.com>
2023-10-22 17:40:00 +0200

    Add ExceptionCollector

---
+++ src/GameFinder.Common/ExceptionWrappers/ExceptionCollector.cs
@@ -0,0 +1,179 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using JetBrains.Annotations;
+
+namespace GameFinder.Common;
+
+/// <summary>
+/// Represents an exception collector.
+/// </summary>
+[PublicAPI]
+public class ExceptionCollector
+{
+    private readonly List<Exception> _exceptions = new();
+
+    /// <summary>
+    /// Checks whether this instance contains any exceptions.
+    /// </summary>
+    /// <returns><c>true</c> if this instance contains any exceptions.</returns>
+    /// <seealso cref="GetExceptions"/>
+    [Pure]
+    [MustUseReturnValue]
+    public bool HasExceptions() => _exceptions.Count != 0;
+
+    /// <summary>
+    /// Returns all collected exceptions in insertion order.
+    /// </summary>
+    /// <seealso cref="HasExceptions"/>
+    [Pure]
+    [MustUseReturnValue]
+    public IReadOnlyList<Exception> GetExceptions() => _exceptions;
+
+    /// <summary>
+    /// Calls the given <paramref name="operation"/> and returns a new
+    /// instance of <see cref="ReferenceTypeOrException{TValue}"/> that
+    /// contains either the result of <paramref name="operation"/> or
+    /// an exception thrown during execution.
+    /// </summary>
+    /// <param name="operation">The operation to call.</param>
+    /// <typeparam name="TValue">The result type of <paramref name="operation"/>.</typeparam>
+    [StackTraceHidden]
+    public ReferenceTypeOrException<TValue> WrapReferenceType<TValue>(
+        Func<TValue> operation) where TValue : class
+    {
+        try
+        {
+            return new ReferenceTypeOrException<TValue>(operation());
+        }
+        catch (Exception e)
+        {
+            _exceptions.Add(e);
+            return new ReferenceTypeOrException<TValue>(e);
+        }
+    }
+
+    /// <summary>
+    /// Calls the given <paramref name="operation"/> with the provided
+    /// <paramref name="state"/> and returns a new
+    /// instance of <see cref="ReferenceTypeOrException{TValue}"/> that
+    /// contains either the result of <paramref name="operation"/> or
+    /// an exception thrown during execution.
+    /// </summary>
+    /// <param name="state">The state to be passed to <paramref name="operation"/>.</param>
+    /// <param name="operation">The operation to call.</param>
+    /// <typeparam name="TState">The type of <paramref name="state"/>.</typeparam>
+    /// <typeparam name="TValue">The result type of <paramref name="operation"/>.</typeparam>
+    [StackTraceHidden]
+    public ReferenceTypeOrException<TValue> WrapReferenceType<TState, TValue>(
+        TState state, Func<TState, TValue> operation) where TValue : class
+    {
+        try
+        {
+            return new ReferenceTypeOrException<TValue>(operation(state));
+        }
+        catch (Exception e)
+        {
+            _exceptions.Add(e);
+            return new ReferenceTypeOrException<TValue>(e);
+        }
+    }
+
+    /// <summary>
+    /// Calls the given <paramref name="operation"/> and returns a new
+    /// instance of <see cref="ValueTypeOrException{TValue}"/> that
+    /// contains either the result of <paramref name="operation"/> or
+    /// an exception thrown during execution.
+    /// </summary>
+    /// <param name="operation">The operation to call.</param>
+    /// <typeparam name="TValue">The result type of <paramref name="operation"/>.</typeparam>
+    [StackTraceHidden]
+    public ValueTypeOrException<TValue> WrapValueType<TValue>(
+        Func<TValue> operation) where TValue : struct
+    {
+        try
+        {
+            return new ValueTypeOrException<TValue>(operation());
+        }
+        catch (Exception e)
+        {
+            _exceptions.Add(e);
+            return new ValueTypeOrException<TValue>(e);
+        }
+    }
+
+    /// <summary>
+    /// Calls the given <paramref name="operation"/> with the provided
+    /// <paramref name="state"/> and returns a new
+    /// instance of <see cref="ValueTypeOrException{TValue}"/> that
+    /// contains either the result of <paramref name="operation"/> or
+    /// an exception thrown during execution.
+    /// </summary>
+    /// <param name="state">The state to be passed to <paramref name="operation"/>.</param>
+    /// <param name="operation">The operation to call.</param>
+    /// <typeparam name="TState">The type of <paramref name="state"/>.</typeparam>
+    /// <typeparam name="TValue">The result type of <paramref name="operation"/>.</typeparam>
+    [StackTraceHidden]
+    public ValueTypeOrException<TValue> WrapValueType<TState, TValue>(
+        TState state, Func<TState, TValue> operation) where TValue : struct
+    {
+        try
+        {
+            return new ValueTypeOrException<TValue>(operation(state));
+        }
+        catch (Exception e)
+        {
+            _exceptions.Add(e);
+            return new ValueTypeOrException<TValue>(e);
+        }
+    }
+
+    /// <summary>
+    /// Calls the given <paramref name="operation"/> and returns a new
+    /// instance of <see cref="UnmanagedTypeOrException{TValue}"/> that
+    /// contains either the result of <paramref name="operation"/> or
+    /// an exception thrown during execution.
+    /// </summary>
+    /// <param name="operation">The operation to call.</param>
+    /// <typeparam name="TValue">The result type of <paramref name="operation"/>.</typeparam>
+    [StackTraceHidden]
+    public UnmanagedTypeOrException<TValue> WrapUnmanagedType<TValue>(
+        Func<TValue> operation) where TValue : unmanaged
+    {
+        try
+        {
+            return new UnmanagedTypeOrException<TValue>(operation());
+        }
+        catch (Exception e)
+        {
+            _exceptions.Add(e);
+            return new UnmanagedTypeOrException<TValue>(e);
+        }
+    }
+
+    /// <summary>
+    /// Calls the given <paramref name="operation"/> with the provided
+    /// <paramref name="state"/> and returns a new
+    /// instance of <see cref="UnmanagedTypeOrException{TValue}"/> that
+    /// contains either the result of <paramref name="operation"/> or
+    /// an exception thrown during execution.
+    /// </summary>
+    /// <param name="state">The state to be passed to <paramref name="operation"/>.</param>
+    /// <param name="operation">The operation to call.</param>
+    /// <typeparam name="TState">The type of <paramref name="state"/>.</typeparam>
+    /// <typeparam name="TValue">The result type of <paramref name="operation"/>.</typeparam>
+    [StackTraceHidden]
+    public UnmanagedTypeOrException<TValue> WrapUnmanagedType<TState, TValue>(
+        TState state, Func<TState, TValue> operation) where TValue : unmanaged
+    {
+        try
+        {
+            return new UnmanagedTypeOrException<TValue>(operation(state));
+        }
+        catch (Exception e)
+        {
+            _exceptions.Add(e);
+            return new UnmanagedTypeOrException<TValue>(e);
+        }
+    }
+}
---
+++ tests/GameFinder.Common.Tests/ExceptionWrappers/ExceptionCollectorTests.cs
@@ -0,0 +1,141 @@
+namespace GameFinder.Common.Tests;
+
+public class ExceptionCollectorTests
+{
+    private record TestClass;
+
+    private const int State = 0;
+    private const int TestValueType = 1;
+    private const int TestUnmanagedType = 1;
+    private static readonly TestClass TestReferenceType = new();
+
+    private static int GetValueType() => TestValueType;
+    private static int GetUnmanagedType() => TestUnmanagedType;
+    private static TestClass GetReferenceType() => TestReferenceType;
+    private static T ThrowException<T>() => throw new NotSupportedException("Testing");
+
+    private static int GetValueType(int _) => TestValueType;
+    private static int GetUnmanagedType(int _) => TestUnmanagedType;
+    private static TestClass GetReferenceType(int _) => TestReferenceType;
+    private static T ThrowException<T>(int _) => throw new NotSupportedException("Testing");
+
+    private static void AssertNoException(ExceptionCollector collector)
+    {
+        collector.HasExceptions().Should().BeFalse();
+        collector.GetExceptions().Should().BeEmpty();
+    }
+
+    private static void AssertHasException(ExceptionCollector collector, Exception exception)
+    {
+        collector.HasExceptions().Should().BeTrue();
+        collector.GetExceptions().Should().ContainSingle().Which.Should().Be(exception);
+    }
+
+    [Fact]
+    public void Test_WrapReferenceType_WithValue()
+    {
+        var collector = new ExceptionCollector();
+
+        _ = collector.WrapReferenceType(GetReferenceType);
+        AssertNoException(collector);
+    }
+
+    [Fact]
+    public void Test_WrapReferenceType_WithException()
+    {
+        var collector = new ExceptionCollector();
+
+        var res = collector.WrapReferenceType(ThrowException<TestClass>);
+        AssertHasException(collector, res.GetException());
+    }
+
+    [Fact]
+    public void Test_WrapReferenceType_WithState_WithValue()
+    {
+        var collector = new ExceptionCollector();
+
+        _ = collector.WrapReferenceType(State, GetReferenceType);
+        AssertNoException(collector);
+    }
+
+    [Fact]
+    public void Test_WrapReferenceType_WithState_WithException()
+    {
+        var collector = new ExceptionCollector();
+
+        var res = collector.WrapReferenceType(State, ThrowException<TestClass>);
+        AssertHasException(collector, res.GetException());
+    }
+
+    [Fact]
+    public void Test_WrapValueType_WithValue()
+    {
+        var collector = new ExceptionCollector();
+
+        _ = collector.WrapValueType(GetValueType);
+        AssertNoException(collector);
+    }
+
+    [Fact]
+    public void Test_WrapValueType_WithException()
+    {
+        var collector = new ExceptionCollector();
+
+        var res = collector.WrapValueType(ThrowException<int>);
+        AssertHasException(collector, res.GetException());
+    }
+
+    [Fact]
+    public void Test_WrapValueType_WithState_WithValue()
+    {
+        var collector = new ExceptionCollector();
+
+        _ = collector.WrapValueType(State, GetValueType);
+        AssertNoException(collector);
+    }
+
+    [Fact]
+    public void Test_WrapValueType_WithState_WithException()
+    {
+        var collector = new ExceptionCollector();
+
+        var res = collector.WrapValueType(State, ThrowException<int>);
+        AssertHasException(collector, res.GetException());
+    }
+
+    [Fact]
+    public void Test_WrapUnmanaged_WithValue()
+    {
+        var collector = new ExceptionCollector();
+
+        _ = collector.WrapUnmanagedType(GetUnmanagedType);
+        AssertNoException(collector);
+    }
+
+    [Fact]
+    public void Test_WrapUnmanaged_WithException()
+    {
+        var collector = new ExceptionCollector();
+
+        var res = collector.WrapUnmanagedType(ThrowException<int>);
+        AssertHasException(collector, res.GetException());
+    }
+
+    [Fact]
+    public void Test_WrapUnmanaged_WithState_WithValue()
+    {
+        var collector = new ExceptionCollector();
+
+        _ = collector.WrapUnmanagedType(State, GetUnmanagedType);
+        AssertNoException(collector);
+    }
+
+    [Fact]
+    public void Test_WrapUnmanaged_WithState_WithException()
+    {
+        var collector = new ExceptionCollector();
+
+        var res = collector.WrapUnmanagedType(State, ThrowException<int>);
+        AssertHasException(collector, res.GetException());
+    }
+}
