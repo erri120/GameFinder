087b1bbb360264f102c2d3b2b2449378ffa57cf2
erri120 <erri120@protonmail.com>
2024-04-05 17:11:50 +0200

    Cleanup

--- src/GameFinder.StoreHandlers.GOG/GOGHandler.cs
+++ src/GameFinder.StoreHandlers.GOG/GOGHandler.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Linq;
-using System.Numerics;
 using GameFinder.Common;
 using GameFinder.RegistryUtils;
 using JetBrains.Annotations;
